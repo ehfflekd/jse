bc61fbeccc082530d054dfdcf55fdeed82bffd74
onsoftel <onsoftel@hanmail.net>
2015-05-15 11:48:44 +0900

    gugudan

---
+++ jse/src/cmm04/array/GugudanMe.java
@@ -0,0 +1,11 @@
+package cmm04.array;
+
+public class GugudanMe {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 2; j <= 5; j++) {
+				System.out.println("");
+			}
+		}
+	}
+}
---
+++ jse/src/cmm04/array/No05_GugudanDemo.java
@@ -0,0 +1,25 @@
+package cmm04.array;
+
+public class No05_GugudanDemo {
+	public static void main(String[] args) {
+		
+	      for(int i=1; i<=9; i++) {
+	            for(int j=2; j<=5; j++) {
+	                System.out.printf("%d * %d = %2d", j, i, (j*i));
+	                System.out.print("\t");
+	            }
+	            System.out.println();
+	        }
+	 
+	        System.out.println();
+	 
+	        for(int i=1; i<=9; i++) {
+	            for(int j=6; j<=9; j++) {
+	                System.out.printf("%d * %d = %2d", j, i, (j*i));
+	                System.out.print("\t");
+	            }
+	            System.out.println();
+	        }
+	    }
+	 
+	}
