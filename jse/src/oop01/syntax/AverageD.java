2812befc92f0908c0b1bc45f5bd66496ad4fcbf8
onsoftel <onsoftel@hanmail.net>
2015-05-19 11:50:53 +0900

    get set..

--- jse/src/oop01/syntax/AverageD.java
+++ jse/src/oop01/syntax/AverageD.java
@@ -1,44 +1,11 @@
 package oop01.syntax;
 
-public class AverageB {
-	/*필드 선언*/
-	String name;
-	int kor;
-	int eng;
-	int tot;
-	double avg; // 합계
-	
-	/*이름 저장하는 기능*/
-	public void setName(String foo){
-		this.name = foo; //매개변수 파라미터 값 
-	}
-	/*국어 점수를 저장하는 기능*/
-	public void setkor(int kor){
-		this.kor = kor;
-	}
-	/*영어 점수를 저장하는 기능*/
-	public void seteng(int eng){
-		this.eng = eng;
-	}
-	/*합계를 구하는 기능*/
-	public void settot(int kor, int eng){
-		this.tot = kor + eng; // 배치는 여기서 따로 할수 있다.
-	}
-	/*평균점수를 구하는 기능*/
-	public void setavg(int tot){
-		this.avg = tot/2;
-	}
-	/*평균 점수를 구하는 기능 2...국어 영어점수 두개만으로 구하기*/
-	/*Overloading " 오버로딩*/
-	public void setavg(int kor, int eng){
-		this.avg = (kor+eng)/2;
-	}
-	
+public class AverageD {
 	public static void main(String[] args) {
 		/*학생 객체 생성 및 메모리 할당*/
-		AverageA hulk = new AverageA();
-		AverageA iron = new AverageA();
-		AverageA hawk = new AverageA();
+		AverageC hulk = new AverageC();
+		AverageC iron = new AverageC();
+		AverageC hawk = new AverageC();
 		/* 이름 저장*/
 		hulk.name = "헐크";
 		iron.name = "아이언";
